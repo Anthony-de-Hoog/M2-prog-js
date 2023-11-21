09a61baf7906e4e4a8531684fb66fb24dd49781f
Anthony de Hoog <37657@ma-web.nl>
2023-11-21 15:12:07 +0100

    02 gedaan

--- 01A/app.js
+++ 01A/app.js
@@ -3,6 +3,21 @@ class App
     runApplication()
     {
         console.log("Hello world!");
+        let appNaam = "Tander";
+        let versienummer = 0.9
+        let versiedatum = 1.5;
+        let autheur = "Anthony Anthonius Antoinette de Hoog";
+        let copyright = "AAA industries";
+        let distributeur = "Nintendo";
+        let darkmode = true;
+
+        console.log(appNaam);
+        console.log(versienummer);
+        console.log(versiedatum);
+        console.log(autheur);
+        console.log(copyright);
+        console.log(distributeur);
+        console.log(darkmode);
 
     }
 }
